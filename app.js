8df1c8fe9bb74367774eb594d78c435a5bbfdc82
Conrad <theradistone@gmail.com>
2019-03-07 13:02:21 -0500

    begin models

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const { db } = require('./models');
 const app = express();
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
