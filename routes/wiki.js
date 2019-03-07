872baba5eef789b7f5c83f45871d86252739b1c1
Conrad <theradistone@gmail.com>
2019-03-07 16:56:40 -0500

    extended routes

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const router = express.Router();
-
+const addPage = require('../views/addPage')
 const layout = require('../views/layout');
+const { Page } = require("../models");
 
 router.get('/', (req, res, next) => {
-
+  res.redirect('/')
 });
 
-router.post('/', (req, res, next) => {
-
+router.post('/', async (req, res, next) => {
+  const page = new Page({
+    title: req.body.title,
+    content: req.body.content
+  });
+  try {
+    await page.save();
+    res.redirect('/');
+  } catch (error) { next(error) }
 });
 
 router.get('/add', (req, res, next) => {
-
+  res.send(addPage())
 });
 
 module.exports = router;
